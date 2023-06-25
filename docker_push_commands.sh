36c5148d584243e0c71057de519eee54c89ace75
frenjaso <60863202+frenjaso@users.noreply.github.com>
2023-06-25 07:46:14 -0700

    update push commands

--- docker_push_commands.sh
+++ docker_push_commands.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 601766089601.dkr.ecr.us-east-1.amazonaws.com
-docker build -t house-data-web-app-repo .
-docker tag house-data-web-app-repo:latest 601766089601.dkr.ecr.us-east-1.amazonaws.com/house-data-web-app-repo:latest
-docker push 601766089601.dkr.ecr.us-east-1.amazonaws.com/house-data-web-app-repo:latest
+docker build -t house-data-web-app .
+docker tag house-data-web-app:latest 601766089601.dkr.ecr.us-east-1.amazonaws.com/house-data-web-app:latest
+docker push 601766089601.dkr.ecr.us-east-1.amazonaws.com/house-data-web-app:latest
