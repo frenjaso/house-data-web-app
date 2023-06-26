de285d1c9a280a28931b7295e1b0e59b5a781a68
frenjaso <60863202+frenjaso@users.noreply.github.com>
2023-06-25 20:31:55 -0700

    Updating docker push script

--- docker_push_commands.sh
+++ docker_push_commands.sh
@@ -2,5 +2,6 @@
 
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 601766089601.dkr.ecr.us-east-1.amazonaws.com
 docker build -t house-data-web-app .
+docker create -it -p 3000:3000 house-data-web-app
 docker tag house-data-web-app:latest 601766089601.dkr.ecr.us-east-1.amazonaws.com/house-data-web-app:latest
 docker push 601766089601.dkr.ecr.us-east-1.amazonaws.com/house-data-web-app:latest
